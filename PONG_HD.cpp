2523acf60d58539839fcbe4b7c4bd4e5b345a2c1
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2021-09-18 14:56:49 +0200

    fixed wrong name on sfml-audio-d-lib on correct sfml-audio.lib + add some comments

--- PONG_HD.cpp
+++ PONG_HD.cpp
@@ -3,17 +3,22 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "PONG_HD");
+
+	window.setFramerateLimit(60);
+
 	while (window.isOpen())
 	{
 		sf::Event event;
 		//main game loop
 		while (window.pollEvent(event))
 		{
-			//process events
+			//Process events
 			if (event.type == sf::Event::Closed)
 			{
 				window.close();
 			}
+			//logic handling
+			
 
 			//Rendering part of the main game loop
 			
